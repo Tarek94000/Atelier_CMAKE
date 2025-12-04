1af8c52a4f3499fea6e9c236504e33fcbd6a605b
Tarek <128355723+Tarek94000@users.noreply.github.com>
2025-12-04 14:50:02 +0100

    Refactor main function for two arguments and square
    Updated main function to accept two arguments and added support for the 'square' operation.

--- src/main.c
+++ src/main.c
@@ -3,33 +3,37 @@
 #include <string.h>
 #include "./../include/calculator.h"
 
-int main(int argc, char *argv[]){
-	if (argc == 4)
-	{
-	char* op = argv[1];
-	char* a = argv[2];
-	char* b = argv[3];
-	double r = 0;
-	if (strcmp(op, "add") == 0) {
-		r = _add(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "sub") == 0) {
-		r = _sub(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "mul") == 0) {
-		r = _mul(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "div") == 0) {
-		r = _div(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else {printf("Erreur de parametres");}
-	}
-	else {printf("Erreur de parametres");}
-	return 0;
-}
+int main(int argc, char *argv[]) {
+    if (argc == 3) {
+        char* op = argv[1];
+        char* a = argv[2];
+        double r = 0;
 
-		
+        if (strcmp(op, "add") == 0) {
+            r = _add(atof(a), atof(argv[3]));
+            printf("%lf\n", r);
+        }
+        else if (strcmp(op, "sub") == 0) {
+            r = _sub(atof(a), atof(argv[3]));
+            printf("%lf\n", r);
+        }
+        else if (strcmp(op, "mul") == 0) {
+            r = _mul(atof(a), atof(argv[3]));
+            printf("%lf\n", r);
+        }
+        else if (strcmp(op, "div") == 0) {
+            r = _div(atof(a), atof(argv[3]));
+            printf("%lf\n", r);
+        }
+        else if (strcmp(op, "square") == 0) {
+            r = square(atof(a));
+            printf("%lf\n", r);
+        }
+        else {
+            printf("Erreur de parametres\n");
+        }
+    } else {
+        printf("Erreur de parametres\n");
+    }
+    return 0;
+}
